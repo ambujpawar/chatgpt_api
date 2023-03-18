c56a510d098f63d9a30cc4f0d1fb289b3884b4a3
Ambuj Pawar <pawar.ambuj@gmail.com>
2023-03-18 20:43:21 +0100

    ADD: Sentdex code

--- src/chatgpt_api/summarize.py
+++ src/chatgpt_api/summarize.py
@@ -5,3 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("API_KEY")
 
+completion = openai.ChatCompletion(
+    engine="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "What is the circumference in km of the planet Earth?"}]
+)
+
+print(completion)
+
+reply_content = completion.choices[0].message.content
+print(reply_content)
+
