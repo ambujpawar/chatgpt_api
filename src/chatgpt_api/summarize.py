06f8a1371e3c03434930d9a97f7c42ad4ceb9ee2
Ambuj Pawar <pawar.ambuj@gmail.com>
2023-03-26 17:04:24 +0200

    ADD: First draft of summarizer

--- src/chatgpt_api/summarize.py
+++ src/chatgpt_api/summarize.py
@@ -1,17 +1,35 @@
 import os
-import openai
+# Third party imports
 from dotenv import load_dotenv
+import gradio as gr
+import openai
 
 load_dotenv()
 openai.api_key = os.getenv("API_KEY")
 
-completion = openai.ChatCompletion(
-    engine="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "What is the circumference in km of the planet Earth?"}]
-)
 
-print(completion)
+def chatgpt_summarize(user_input: str, role: str = "user"):
+    messages=[
+        {"role": "user", "content": "You are a summarizer bot. I provide you with a long text and you summarize it"},
+        {"role": "assistant", "content": "OK!"}
+        ]
+    messages.append({"role": role, "content": user_input})
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", #This is 0301 model
+        messages=messages,
+    )
+    reply_content = completion.choices[0].message.content
+    return reply_content
 
-reply_content = completion.choices[0].message.content
-print(reply_content)
 
+def summarize_text(text):
+    summary = chatgpt_summarize(text, role="user")
+    return summary
+
+
+if __name__ == "__main__":
+    input_text = gr.inputs.Textbox(label="Input Text")
+    output_text = gr.outputs.Textbox(label="Summary")
+
+    demo = gr.Interface(fn=summarize_text, inputs=input_text, outputs=output_text, title="Summarizer App", description="Enter a long text and get a summary.").launch()
+    demo.launch()
